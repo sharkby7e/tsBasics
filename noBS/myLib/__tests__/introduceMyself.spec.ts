58f2f2232e4948c73e06cc15dc052b9bd3a8635b
sharkbyte <sgquin@gmail.com>
2022-08-03 17:32:23 -0700

    finished set testing suite

--- noBS/myLib/__tests__/introduceMyself.spec.ts
+++ noBS/myLib/__tests__/introduceMyself.spec.ts
@@ -8,7 +8,34 @@ describe("introduceMyself", () => {
 
 describe("Set", () => {
   it("should return true", () => {
-    const set = Set;
-    expect(set.isEmpty).toEqual(true);
+    const set = new Set();
+    expect(set.isEmpty()).toEqual(true);
+  });
+
+  it("should not be empty after you add a string", () => {
+    const set = new Set();
+    set.add("test");
+    expect(set.isEmpty()).toBe(false);
+  });
+
+  it("should return whether or not a certain string is in the set", () => {
+    const set = new Set();
+    set.add("Sid");
+    expect(set.contains("Sid")).toBe(true);
+  });
+
+  it("should only contain strings that I have added", () => {
+    const set = new Set();
+    set.add("Sid");
+    expect(set.contains("Ash")).toBe(false);
+  });
+
+  it("should remove a string when I call remove", () => {
+    const set = new Set();
+    set.add("Sid");
+    expect(set.contains("Sid")).toBe(true);
+    set.remove("Sid");
+    expect(set.contains("Sid")).toBe(false);
+    expect(set.isEmpty()).toBe(true);
   });
 });
--- noBS/myLib/src/index.ts
+++ noBS/myLib/src/index.ts
@@ -1,9 +1,17 @@
-export const introduceMyself = (first: string) => `Hello ${first}`;
+export const introduceMyself = (first: string): string => `Hello ${first}`;
 
-export const Set = {
-  isEmpty: () => {
-    return true;
-  },
-};
-
-module.exports = { Set };
+export class Set {
+  list = [];
+  isEmpty = function (): boolean {
+    return this.list.length === 0;
+  };
+  add = function (string: string): void {
+    this.list.push(string);
+  };
+  contains = function (string: string): boolean {
+    return this.list.includes(string);
+  };
+  remove = function (string: string): void {
+    this.list.splice(this.list.indexOf(string), 1);
+  };
+}
